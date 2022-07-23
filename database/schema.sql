ac2f3191d41ba00f423a8076d40880d92310e365
YuriXbr <claudemiranjos67@gmail.com>
2022-07-23 12:50:52 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,18 +1,35 @@
 CREATE DATABASE discord;
 
 
-CREATE TABLE Guilds (
-        guildId         VARCHAR(100)        NOT             NULL        PRIMARY KEY,
-        guildOwnerId    VARCHAR(100)        NOT             NULL
-);
+CREATE TABLE `guildconfigurable` (
+	`guildId` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`cmdPrefix` VARCHAR(10) NULL DEFAULT '!' COLLATE 'utf8mb4_0900_ai_ci',
+	`modlogId` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`guildId`) USING BTREE
+)
 
-CREATE TABLE GuildConfigurable (
-        guildId         VARCHAR(100)        NOT             NULL        PRIMARY KEY,
-        cmdPrefix       VARCHAR(10)         DEFAULT '!',    
-        modlogId        VARCHAR(100)
-);
+CREATE TABLE `guilds` (
+	`guildId` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`guildOwnerId` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`guildId`) USING BTREE
+)
 
 CREATE TABLE `playlists` (
-	playlistname    TEXT                NULL            DEFAULT     NULL,
-	playlistlink    TEXT                NULL            DEFAULT     NULL 
-);
+	`playlistname` VARCHAR(500) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`playlistlink` VARCHAR(500) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`description` VARCHAR(500) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci'
+)
+
+CREATE TABLE `queuemessagetoggle` (
+	`guildId` VARCHAR(256) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`chave` VARCHAR(256) NULL DEFAULT '1' COLLATE 'utf8mb4_0900_ai_ci',
+	`forcar` VARCHAR(256) NULL DEFAULT '0' COLLATE 'utf8mb4_0900_ai_ci'
+)
+
+CREATE TABLE `staffid` (
+	`id` VARCHAR(50) NOT NULL DEFAULT '297905031171145751' COLLATE 'utf8mb4_0900_ai_ci',
+	`dj` TINYINT(1) NOT NULL DEFAULT '0',
+	`permissionlevel` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_0900_ai_ci',
+	`note` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
