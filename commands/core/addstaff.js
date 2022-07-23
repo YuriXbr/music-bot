e192cbf7ad0932c96ce17d15931048bcb5da9df4
YuriXbr <claudemiranjos67@gmail.com>
2022-07-23 12:37:42 -0300

    2.4.0

--- commands/core/addstaff.js
+++ commands/core/addstaff.js
@@ -16,9 +16,14 @@ module.exports = {
         connection = await require('../../database/db');
         if(!args[0]) return message.reply('insira o usuario para ser promovido');
         if(args[1] != '1' && args[1] != '0') return message.reply('Valor para DJ invalido');
-        if(args[2] >= '4' || args[2] <= '0') return message.reply('Valor para staff invalido')
+        if(args[2] >= '4' || args[2] <= '-1') return message.reply('Valor para staff invalido')
         if(!member)  return message.reply('Mencione um usuario')
-
+        if(args[1] == '1') _dj = 'Ativo';
+        if(args[1] == '0') _dj = 'Desativo';
+        if(args[2] == '1') _stt = 'Moderador';
+        if(args[2] == '2') _stt = 'Administrador';
+        if(args[2] == '3') _stt = 'Owner';
+        if(args[2] == '0') _stt = 'Membro';
         
         await connection.query(
             `INSERT IGNORE INTO staffid VALUES('${message.mentions.users.first().id}', '${args[1]}', '${args[2]}', null)`
@@ -28,9 +33,7 @@ module.exports = {
             `UPDATE staffid SET id = '${message.mentions.users.first().id}', dj = '${args[1]}', permissionlevel= '${args[2]}' WHERE id = '${message.mentions.users.first().id}'`
         )
         
-
-        
-        
-        
+       
+        message.reply(`Usuario ${member} com os seguintes cargos: \n  DJ: ${_dj} \n  Staff Level: ${_stt}`);
     },
 };
--- commands/core/translate.js
+++ commands/core/translate.js
@@ -1,8 +1,7 @@
 const translate = require('@iamtraction/google-translate');
 const {Client, message, MessageEmbed} = require('discord.js');
-const { randomInt } = require('mathjs');
+const { randomInt, number } = require('mathjs');
 const math = require('mathjs');
-const { reset } = require('nodemon');
 
 module.exports = {
     name: 'translate',
@@ -10,13 +9,14 @@ module.exports = {
     utilisation: '{prefix}translate',
     
     async execute(client, message, args) {
-        const avalible = ['af', 'ar', 'bg', 'bn', 'bs', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'et', 'fi', 'fr', 'gu', 'hi', 'hr', 'hu', 'id', 'is', 'it', 'iw', 'ja', 'jw', 'km', 'kn', 'ko', 'la', 'lv', 'ml', 'mr', 'ms', 'my', 'ne', 'nl', 'no', 'pl', 'pt', 'ro', 'ru', 'si', 'sk', 'sq', 'sr', 'su', 'sv', 'sw', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'vi', 'zh-CN']
-        const list = 'af, ar, bg, bn, bs, ca, cs, da, de, el, en, es, et, fi, fr, gu, hi, hr, hu, id, is, it, iw, ja, jw, km, kn, ko, la, lv, ml, mr, ms, my, ne, nl, no, pl, pt, ro, ru, si, sk, sq, sr, su, sv, sw, ta, te, th, tl, tr, uk, ur, vi, zh-CN';
-        
-        if (args[0] == 'list') return message.reply(list);
-        const query = args.slice(1).join(" "); 
-        if (!query) return message.reply('Indique um texto para ser traduzido')
+        const avalible = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'kn', 'kk', 'km',, 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
+        const list = 'af, sq, am, ar, hy, az, eu, be, bn, bs, bg, ca, ceb, co, hr, cs, da, nl, en, eo, et, fi, fr, fy, gl, ka, de, el, gu, ht, ha, haw, he, hi, hmn, hu, is, ig, id, ga, it, ja, kn, kk, km, ko, ku, ky, lo, la, lv, lt, lb, mk, mg, ms, ml, mt, mi, mr, mn, my, ne, no, ny, or, ps, fa, pl, pt, pa, ro, ru, sm, gd, sr, st, sn, sd, si, sk, sl, so, es, su, sw, sv, tl, tg, ta, tt, te, th, tr, uk, ur, uz, vi, cy, xh, yi, yo, zu';
+        const _limit = avalible.length;
+
+        if (args[0] == 'list') return message.reply(list);''
         if (args[0] != 'hyper') {
+            const query = args.slice(1).join(" "); 
+            if (!query) return message.reply('Indique um texto para ser traduzido')
             translate(query, {from: 'auto', to: args[0]}).then(res => {
                 message.reply(res.text);
                 return;
@@ -25,15 +25,24 @@ module.exports = {
             });
         } 
         if (args[0] == 'hyper') {
+            const query = args.slice(2).join(" "); 
+
+            if (!isNaN(args[1])) {
+                if(args[1] >= '501' || args[1] <= '0') return('valor invalido, deve ser maior que 0 e menor ou igual a 500')
+                _qnt = args[1];
+            } else return message.reply('Informe a quantia de numeros de traduções')
+
+            if (!query) return message.reply('Indique um texto para ser traduzido')
             var hypertranslate = query;
             var replyed = false;
-            const msg = await message.channel.send('processando (tempo estimado: 10s)...'); 
+            const msg = await message.channel.send(`processando (tempo estimado: ${_qnt/2}s)...`); 
             t = 0;
-             while(t < 20) {
-                var tolang = avalible[math.randomInt(0, 30)]
-                var translated = await translate(hypertranslate, {from: 'auto', to: tolang});
+             while(t < _qnt) {
+                var num = math.randomInt(0, 103);
+                var translated = await translate(hypertranslate, {from: 'auto', to: avalible[num]});
                     hypertranslate = translated.text;
                 t ++;
+                console.log(t);
             }
             var translated = await translate(hypertranslate, {from: 'auto', to: 'pt'});
             msg.edit(translated.text);
